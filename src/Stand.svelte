155faa13eb90ae8f9fa2d73c1dfe0c3d2dceac18
reiyayakko <reiyayakko@gmail.com>
2021-02-05 18:52:23 +0900

    feat: add Stand

---
+++ src/Stand.svelte
@@ -0,0 +1,40 @@
+
+<script lang="ts">
+    import type { Player } from "./game/piece";
+    import type { PieceStand } from "./game/piece-stand";
+    import Piece from "./Piece.svelte";
+
+    export let stand: PieceStand;
+    export let player: Player;
+</script>
+
+<div>
+    {#each Object.entries(stand) as [type, amount]}
+        {#if amount > 0}
+            <div class="stand-item">
+                <Piece type={+type} {player} />
+                <span>
+                    {#if amount === Infinity}∞{:else}{amount}{/if}
+                </span>
+            </div>
+        {/if}
+    {/each}
+</div>
+
+<style lang="scss">
+    @use "env";
+    .stand-item {
+        width: env.$size;
+        height: env.$size;
+        display: inline-block;
+        position: relative;
+    }
+    span {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        margin: 4px 10px;
+        $shadow: 0 0 4px whitesmoke;
+        text-shadow: $shadow, $shadow, $shadow, $shadow;
+    }
+</style>
